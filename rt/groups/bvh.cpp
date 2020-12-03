358fd1aaff3bb55a56366a2049c9a2f56bd972db
kate00001 <kate00001@stud.uni-saarland.de>
2020-12-03 07:59:20 +0100

    Update 'rt/groups/bvh.cpp'

--- rt/groups/bvh.cpp
+++ rt/groups/bvh.cpp
@@ -258,6 +258,26 @@ namespace rt {
     void BVH::setCoordMapper(CoordMapper* cm) {
         /* TODO */ NOT_IMPLEMENTED;
     }
+  
+	void BVH::serialize(BVH::Output& output) {
+    	// To implement this function:
+    	// - Call output.setNodeCount() with the number of nodes in the BVH
+   	 	/* TODO */
+    	// - Set the root node index using output.setRootId()
+    	/* TODO */
+    	// - Write each and every one of the BVH nodes to the output using output.writeNode()
+    	/* TODO */ NOT_IMPLEMENTED;
+	}
+	void BVH::deserialize(BVH::Input& input) {
+    	// To implement this function:
+    	// - Allocate and initialize input.getNodeCount() nodes
+    	/* TODO */
+    	// - Fill your nodes with input.readNode(index)
+    	/* TODO */
+    	// - Use the node at index input.getRootId() as the root node
+    	/* TODO */ NOT_IMPLEMENTED;
+	}
+
    float BVH::getArea() const {
         return 0;
     }
