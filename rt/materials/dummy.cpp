c9e23a23ba484cf0e2e76f42f5d25f52f229e684
akshay <akmu00001@stud.uni-saarland.de>
2020-12-12 18:59:25 +0530

    dummy material implementation

--- rt/materials/dummy.cpp
+++ rt/materials/dummy.cpp
@@ -8,11 +8,11 @@ DummyMaterial::DummyMaterial() {
 }
 
 RGBColor DummyMaterial::getReflectance(const Point& texPoint, const Vector& normal, const Vector& outDir, const Vector& inDir) const {
-    /* TODO */ NOT_IMPLEMENTED;
+	return RGBColor::rep(1.0f);
 }
 
 RGBColor DummyMaterial::getEmission(const Point& texPoint, const Vector& normal, const Vector& outDir) const {
-    /* TODO */ NOT_IMPLEMENTED;
+	return RGBColor::rep(0.0f);
 }
 
 Material::SampleReflectance DummyMaterial::getSampleReflectance(const Point& texPoint, const Vector& normal, const Vector& outDir) const {
