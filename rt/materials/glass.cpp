c84b821a1bcfe5083c486b21553b4e128179a4b1
kate00001 <kate00001@stud.uni-saarland.de>
2021-01-08 01:04:27 +0100

    Update 'rt/materials/glass.cpp'

--- rt/materials/glass.cpp
+++ rt/materials/glass.cpp
@@ -19,6 +19,10 @@ RGBColor GlassMaterial::getEmission(const Point& texPoint, const Vector& normal,
 
 Material::SampleReflectance GlassMaterial::getSampleReflectance(const Point& texPoint, const Vector& normal, const Vector& outDir) const {
 	
+	//TODO: or the competetion, you can implement reflectivity as a function of incident angle, given by Fresnel equation.
+	//check the pdf for the formula.
+
+	//refraction index value depends on whether the ray is entering or leaving a material 
 	float eta_t;
 	if(dot(outDir,normal) > 0.0f)	//incoming ray from above the solid
 		eta_t = eta;
@@ -29,20 +33,22 @@ Material::SampleReflectance GlassMaterial::getSampleReflectance(const Point& tex
 	float cosTheta_i = std::abs(dot(outDir,normal) / (outDir.length()*normal.length()));
 	float cosTheta_t_sqr = 1 - (1 - sqr(cosTheta_i))/sqr(eta_t);
 
+
+	//total internal reflection - all the energy is reflected, non transmitted
 	if(cosTheta_t_sqr < 0.0f)
 	{
-		////total internal reflection - all the energy is reflected, non transmitted
 		//reflected ray
 		Vector reflectedDirection = -outDir + 2 * dot(outDir,normal) * normal;
 		return SampleReflectance(reflectedDirection, RGBColor::rep(1.0f));
 	}
 
-
-	float cosTheta_t = sqrt(cosTheta_t_sqr);
+	//estimate reflectance
+	float cosTheta_t = std::abs(sqrt(cosTheta_t_sqr));
 	float r_parallel = (eta_t*cosTheta_i - cosTheta_t) / (eta_t*cosTheta_i + cosTheta_t);
 	float r_perpendicular = (cosTheta_i - eta_t*cosTheta_t) / (cosTheta_i + eta_t*cosTheta_t);
 	float reflectance = 0.5f * (sqr(r_parallel) + sqr(r_perpendicular));
 
+	//half the rays are reflected, half are refracted
 	if(random() > 0.5f)
 	{
 		//reflected ray
@@ -51,15 +57,19 @@ Material::SampleReflectance GlassMaterial::getSampleReflectance(const Point& tex
 	}
 	else
 	{
-		//refracted ray
-		Vector refractedRay = ((cosTheta_i*normal - outDir) / eta_t) - normal * cosTheta_t;
+		int sign = (dot(outDir,normal)>0) ? 1 : -1;
 
-		//not sure about the reason behind this, the Drone CI tests suggest this
+		//refracted ray
+		Vector refractedRay = ((cosTheta_i*normal*sign - outDir) / eta_t) - normal * sign * cosTheta_t;
+		//note: we needed to multiply sign variable with normal above because normal faces opposite directions wrt outDir when a ray enter and leaves a material 
+		//see the derivation sheet attached
+
+		//transmittance = (1 - reflectance) / sqr(eta of the refracted ray material)
 		float transmittance;
 		if(dot(outDir,normal) > 0.0f)
 			transmittance = (1.0f-reflectance)/sqr(eta_t);
 		else
-			transmittance = 1.0f-reflectance;
+			transmittance = 1.0f-reflectance;		//eta of refracted ray material = 1
 
 		return SampleReflectance(refractedRay, 2*RGBColor::rep(transmittance));			//multiplied by 2 to compensate for the energy loss, when not considering the other ray
 	}
