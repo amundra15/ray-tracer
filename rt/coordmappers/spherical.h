ff820a4d89ea54257a98d3b912de937506e06af4
kate00001 <kate00001@stud.uni-saarland.de>
2021-01-10 01:52:05 +0100

    Update 'rt/coordmappers/spherical.h'

--- rt/coordmappers/spherical.h
+++ rt/coordmappers/spherical.h
@@ -1,26 +1,29 @@
 #ifndef CG1RAYTRACER_COORDMAPPERS_SPHERICAL_HEADER
 #define CG1RAYTRACER_COORDMAPPERS_SPHERICAL_HEADER
 
-#include <core/point.h>
-#include <core/vector.h>
 #include <rt/coordmappers/coordmapper.h>
+#include <core/vector.h>
 #include <core/matrix.h>
 
 namespace rt {
 
-class Intersection;
+class Vector;
+
 class SphericalCoordMapper : public CoordMapper {
 public:
-    SphericalCoordMapper();
     SphericalCoordMapper(const Point& origin, const Vector& zenith, const Vector& azimuthRef);
     virtual Point getCoords(const Intersection& hit) const;
+
 private:
-    Point origin;
-    Vector zenith; //Y
-    Vector azimuthRef; //X
-    Vector thirdZ; //z
-    float aScale, zScale;
-    Matrix transformation;
+    Point center;
+    Vector zenith;
+    Vector azimuthRef;
+    Matrix base;
+
+    float scaleX;
+    float scaleY;
+
+
 };
 
 }
