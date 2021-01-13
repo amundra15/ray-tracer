d6359da18096e2b96e9e152d906fa8ffd53554f8
Kartik Teotia <kartikt4997@gmail.com>
2021-01-13 20:47:36 +0100

    added envmap

--- main/main.cpp
+++ main/main.cpp
@@ -13,10 +13,10 @@ void a_smooth();
 void a_bumpmappers();
 
 int main(int argc, char* argv[]) {
-    a_materials();
-    //a_textures();
+    //a_materials();
+    a_textures();
     //a_local();
-    a_mappers();
+    //a_mappers();
     //a_distributed();
     //a_smooth();
     return 0;
