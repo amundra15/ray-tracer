3df9941140c11ecd9e540aa837044456b5fa22e3
akshay <akmu00001@stud.uni-saarland.de>
2021-01-27 12:39:35 +0530

    measure code execution time

--- main/main.cpp
+++ main/main.cpp
@@ -1,8 +1,19 @@
+#include <iostream>
+#include <chrono> 
+
 void a_scene();
+
+using namespace std::chrono; 
 
 int main(int argc, char* argv[]) {
     
+    auto start = high_resolution_clock::now(); 
+
     a_scene();
+
+    auto stop = high_resolution_clock::now(); 
+    auto duration = duration_cast<milliseconds>(stop - start); 
+    std::cout << float(duration.count())/1000.0 << "seconds" << std::endl; 
     
     return 0;
 }
