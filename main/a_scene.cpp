c0e03649c836c1579de0778ecb1a0d017fb78aef
kate00001 <kate00001@stud.uni-saarland.de>
2021-01-28 03:15:17 +0100

    Update 'main/a_scene.cpp'

--- main/a_scene.cpp
+++ main/a_scene.cpp
@@ -1,4 +1,5 @@
 
+
 #include <core/image.h>
 #include <core/color.h>
 #include <core/random.h>
@@ -16,7 +17,6 @@
 #include <rt/solids/quad.h>
 #include <rt/solids/sphere.h>
 #include <rt/solids/triangle.h>
-#include <rt/solids/environment.h>
 
 #include <rt/textures/constant.h>
 #include <rt/textures/imagetex.h>
@@ -35,8 +35,6 @@
 #include <rt/lights/pointlight.h>
 #include <rt/lights/spotlight.h>
 #include <rt/lights/directional.h>
-
-#include <rt/coordmappers/environment.h>
 
 #include <algorithm>
 
@@ -59,57 +57,173 @@ MatLib* getTeapotMatlib() {
     MatLib* matlib = new MatLib;
     Texture* black1 = new ConstantTexture(RGBColor::rep(0.0f));
     ImageTexture* box4 = new ImageTexture("models/default.png");
-    ImageTexture* box5 = new ImageTexture("models/default.png");
+   ImageTexture* box5 = new ImageTexture("models/dmr.png");
+       ImageTexture* whitetex = new ImageTexture("models/kettle_texture.png");
+           ConstantTexture* blacktex = new ConstantTexture(RGBColor::rep(0.0f));
+               //LambertianMaterial white(blacktex, whitetex);
+
+    //Texture*   whitetex = new ConstantTexture(RGBColor::rep(1.0f));
+
     Texture* light = new ConstantTexture(RGBColor(0.6f, 0.6f, 0.9f) * 0.6);
+    LambertianMaterial* mat = new LambertianMaterial(blacktex,whitetex);
+      CombineMaterial* combined = new CombineMaterial();
+    combined->add(new LambertianMaterial(blacktex, whitetex), 0.5f);
+    //combined->add( new PhongMaterial(box5, 100.0f),0.5f);
+    matlib->insert(std::pair<std::string, Material*>("DefaultMaterial", mat));
+
+    //matlib->insert(std::pair<std::string, Material*>("default", combined));
+   
+    return matlib;
+}
+MatLib* getFloorMatlib() {
+    MatLib* matlib = new MatLib;
+    ImageTexture* box6 = new ImageTexture("models/wood.png");
+     MirrorMaterial* mirror = new MirrorMaterial(0.0f, 0.0f);
+     Texture*    whitetex = new ConstantTexture(RGBColor::rep(1.0f));
+
+    PhongMaterial* phong = new PhongMaterial(whitetex, 10.0f);
+    CombineMaterial* combined = new CombineMaterial();
+        Texture* greytex2 = new ConstantTexture(RGBColor::rep(0.7f));
+            Material* grey_cook_rain = new CookTorranceMaterial(box6, 0.2f, 0.8f, 0.3f,0.3f);
+
+    combined->add(mirror,0.18f);
+    combined->add(phong,0.62f);
+
+    combined->add(grey_cook_rain,0.42f);
+    matlib->insert(std::pair<std::string, Material*>("lambert1",combined));
+
+    return matlib;
+}
+MatLib* getWallMatlib() {
+    MatLib* matlib = new MatLib;
+    ImageTexture* box6 = new ImageTexture("models/wood.png");
+     MirrorMaterial* mirror = new MirrorMaterial(0.0f, 0.0f);
+     Texture*    whitetex = new ConstantTexture(RGBColor::rep(1.0f));
+
+    PhongMaterial* phong = new PhongMaterial(whitetex, 10.0f);
+    CombineMaterial* combined = new CombineMaterial();
+        Texture* greytex2 = new ConstantTexture(RGBColor::rep(0.7f));
+            Material* grey_cook_rain = new CookTorranceMaterial(box6, 0.2f, 0.8f, 0.3f,0.3f);
+
+    combined->add(mirror,0.18f);
+    combined->add(phong,0.62f);
+
+    combined->add(grey_cook_rain,0.42f);
+    matlib->insert(std::pair<std::string, Material*>("lambert1", new CookTorranceMaterial(box6, 0.1, 0.8, 0.2,0.3)));
+
+    return matlib;
+}
+MatLib* getFrameMatlib() {
+    MatLib* matlib = new MatLib;
+       Texture* dirt = new ConstantTexture(RGBColor::rep(0.25f));
+
+    matlib->insert(std::pair<std::string, Material*>("Black",new CookTorranceMaterial(dirt, 0.1, 0.8, 0.2,0.3)));
+    matlib->insert(std::pair<std::string, Material*>("Blue",new CookTorranceMaterial(dirt, 0.1, 0.8, 0.2,0.3)));
+    matlib->insert(std::pair<std::string, Material*>("Cuadro",new CookTorranceMaterial(dirt, 0.1, 0.8, 0.2,0.3)));
+    matlib->insert(std::pair<std::string, Material*>("Cuadro.001",new CookTorranceMaterial(dirt, 0.1, 0.8, 0.2,0.3)));
+    matlib->insert(std::pair<std::string, Material*>("L.Blue",new CookTorranceMaterial(dirt, 0.1, 0.8, 0.2,0.3)));
+    matlib->insert(std::pair<std::string, Material*>("Red",new CookTorranceMaterial(dirt, 0.1, 0.8, 0.2,0.3)));
+    matlib->insert(std::pair<std::string, Material*>("White",new CookTorranceMaterial(dirt, 0.1, 0.8, 0.2,0.3)));
+    matlib->insert(std::pair<std::string, Material*>("Yellow",new CookTorranceMaterial(dirt, 0.1, 0.8, 0.2,0.3)));
+    
+    return matlib;
+}
+MatLib* getBreadMatlib() {
+    MatLib* matlib = new MatLib;
+       ImageTexture* whitetex = new ImageTexture("models/Bread.png");
+           ConstantTexture* blacktex = new ConstantTexture(RGBColor::rep(0.0f));
+     
+    LambertianMaterial* mat = new LambertianMaterial(blacktex,whitetex);
+    matlib->insert(std::pair<std::string, Material*>("Material.002", mat));
+
+    //matlib->insert(std::pair<std::string, Material*>("default", combined));
+   
+   
+    return matlib;
+}
+MatLib* getPlateMatlib() {
+    MatLib* matlib = new MatLib;
+    Texture* whitetex = new ConstantTexture(RGBColor::rep(1.0f));
+    CombineMaterial* combined = new CombineMaterial();
+    combined->add(new LambertianMaterial(whitetex, whitetex), 0.5f);
+    combined->add( new PhongMaterial(whitetex, 100.0f),0.5f);
+    matlib->insert(std::pair<std::string, Material*>("Mat", combined));
+
+    //matlib->insert(std::pair<std::string, Material*>("default", combined));
+   
+   
+    return matlib;
+}
 
 
-    matlib->insert(std::pair<std::string, Material*>("Default", new CookTorranceMaterial(box5, 0.1, 0.8, 0.2,0.3)));
+MatLib* getWineBottleMatlib() {
+        MatLib* matlib = new MatLib;
+        Texture* phong1_bclr = new ConstantTexture(RGBColor(0.035f, 0.252f, 1.0f));
+            matlib->insert(std::pair<std::string, Material*>("wire_225088199",new GlassMaterial(2.0f)));
 
-    matlib->insert(std::pair<std::string, Material*>("default", new CookTorranceMaterial(box5, 0.1, 0.8, 0.2,0.3)));
    
     return matlib;
 }
 void a_scene() {
-        Image img(200,140);
+        Image img(720,720);
 
     std::cout << "Resolution is " << img.width() << ", " << img.height() << std::endl;
     World world;
-    // SimpleGroup* scene = new SimpleGroup();
     BVH* scene = new BVH();
     world.scene = scene;
   
 
     // Main Camera
-    PerspectiveCamera* cam = new PerspectiveCamera(Point(-0.48f, 2.04f, -0.36f), Vector(0.605, -1.6, 1.27), Vector(-0.004, 0.62, 0.78), pi/2, pi/4);
-    
+PerspectiveCamera* cam = new PerspectiveCamera(Point(-0.861, 0.569f, 0.504f), Vector(-0.82+0.86, 0.5643-0.5679, 0.54-0.50), Vector(-0.026, 0.997714, 0.062), pi/2, pi/4);  //Texture* blacktex = new ConstantTexture(RGBColor::rep(0.1f));
+//PerspectiveCamera* cam = new PerspectiveCamera(Point(-0.127f, 0.68f, -1.23f), Vector(-0.107+0.127, -0.029-0.62, 1.12+1.23), Vector(0, 0.96, 0.26), pi/2, pi/4);  //Texture* blacktex = new ConstantTexture(RGBColor::rep(0.1f));
+    ImageTexture* whitetex = new ImageTexture("models/stones_diffuse.png");
+    ConstantTexture* redtex = new ConstantTexture(RGBColor(.7f,0.f,0.f));
+    ConstantTexture* greentex = new ConstantTexture(RGBColor(0.f,.7f,0.f));
+    ConstantTexture* blacktex = new ConstantTexture(RGBColor::rep(0.0f));
+        LambertianMaterial white(blacktex, whitetex);
+
+MatLib* matlib_table = getTableMatlib();
+    loadOBJ(scene, "models/", "Wooden_Table.obj", matlib_table);
+    MatLib* matlib_pot = getTeapotMatlib();
+    MatLib* bottle = getWineBottleMatlib();
+    MatLib* bread = getBreadMatlib();
+    MatLib* plate = getPlateMatlib();
+    loadOBJ(scene,"models/","plate.obj",plate);
+    loadOBJ(scene,"models/","kettle.obj",matlib_pot);
+    loadOBJ(scene,"models/", "Bread.obj",bread);
+    loadOBJ(scene,"models/", "glass_2.obj",bottle);
+//lighting
+    MatLib* frame = getFrameMatlib();
+    //loadOBJ(scene,"models/","frame.obj",frame);
+    MatLib* wall = getWallMatlib();
+    loadOBJ(scene,"models/","wall.obj",wall);
+    MatLib* floor = getFloorMatlib();
+    float scale = 0.01f;
+    //loadOBJ(scene,"models/","floor.obj",wall);
+    //loadOBJ(scene,"models/","mirror_boundary.obj");
+//lighting//wall
+    scene->add(new Triangle(Point(-200.f,000.f,360.f)*scale, Point(-200.f,550.f,360.f)*scale, Point(350.f,000.f,360.f)*scale, nullptr, &white));
+    scene->add(new Triangle(Point(350.f,550.f,360.f)*scale, Point(350.f,000.f,360.f)*scale, Point(-200.f,550.f,360.f)*scale, nullptr, &white));
+
+    scene->add(new Triangle(Point(-200.f,000.f,000.f)*scale, Point(-200.f,000.f,560.f)*scale, Point(350.f,000.f,000.f)*scale, nullptr, &white));
+    scene->add(new Triangle(Point(350.f,000.f,560.f)*scale, Point(350.f,000.f,000.f)*scale, Point(-200.f,000.f,560.f)*scale, nullptr, &white));
 
     RGBColor yellow_clr =  RGBColor(0.467f, 0.14f, 0.09f);
     RGBColor blue_clr = RGBColor(0.035f, 0.252f, 1.0f);
     RGBColor red_clr = RGBColor(1.0f, 0.02f, 0.02f);
-    Texture* blacktex = new ConstantTexture(RGBColor::rep(0.1f));
-    Texture* greytex = new ConstantTexture(RGBColor::rep(0.2f));
-    Texture* whitetex = new ConstantTexture(RGBColor::rep(1.0f));
 
-    MatLib* matlib_table = getTableMatlib();
-    loadOBJ(scene, "models/", "Wooden_Table.obj", matlib_table);
-    MatLib* matlib_pot = getTeapotMatlib();
-    loadOBJ(scene,"models/","teapot.obj",matlib_pot);
+    world.light.push_back(new PointLight(Point(-0.127f, 0.68f, -1.23f), yellow_clr )); // BL1 yellow
 
-    //env mapper
-    world.envSolid = new Environment(new EnvironmentMapper(), new FlatMaterial(new ConstantTexture(blue_clr)));
-    
+    world.light.push_back(new PointLight(Point(-0.116f, 3.86f, 1.09f), yellow_clr )); // BL1 yellow
 
-    //lighting
-    world.light.push_back(new PointLight(Point(-0.35f, 2.18f, 2.6f), RGBColor(1, 1, 1))); // BL1 yellow
-    world.light.push_back(new PointLight(Point(0.35f, 0.18f, 0.6f), yellow_clr * 0.25)); // BL1 yellow
-    world.light.push_back(new DirectionalLight(Vector(0.2f ,-0.5f , 1.0f).normalize(), RGBColor(1.0f, 1.0f, 1.0f) * 0.15f)); // white
+    world.light.push_back(new DirectionalLight(Vector(0.2f ,-0.5f , 1.0f).normalize(), RGBColor(1.0f, 1.0f, 1.0f) )); // white
     world.light.push_back(new DirectionalLight(Vector(-0.2f ,-0.5f , 1.0f).normalize(), yellow_clr * 1.f)); // yellow
 
     RecursiveRayTracingIntegrator integrator(&world);
     scene->rebuildIndex();
 
     Renderer engine(cam, &integrator);
-    // engine.setSamples(25);
+    engine.setSamples(25);
     engine.render(img);
     img.writePNG("scene.png");
 }
