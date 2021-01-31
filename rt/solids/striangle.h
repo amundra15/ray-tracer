f936d5718b1abd5250548a93d1a251e234229333
kate00001 <kate00001@stud.uni-saarland.de>
2021-01-31 23:27:58 +0100

    Update 'rt/solids/striangle.h'

--- rt/solids/striangle.h
+++ rt/solids/striangle.h
@@ -1,3 +1,4 @@
+
 #ifndef CG1RAYTRACER_SOLIDS_STRIANGLE_HEADER
 #define CG1RAYTRACER_SOLIDS_STRIANGLE_HEADER
 
@@ -8,19 +9,19 @@ namespace rt {
 
 class SmoothTriangle : public Triangle {
 public:
+
     SmoothTriangle() {}
     SmoothTriangle(Point vertices[3], Vector normals[3], CoordMapper* texMapper, Material* material);
     SmoothTriangle(const Point& v1, const Point& v2, const Point& v3, 
         const Vector& n1, const Vector& n2, const Vector& n3, 
         CoordMapper* texMapper, Material* material);
 
+    virtual BBox getBounds() const;
     virtual Intersection intersect(const Ray& ray, float previousBestDistance = FLT_MAX) const;
-    CoordMapper* texMapper;
-    Material* material;
-     virtual BBox getBounds() const;
-    Triangle t1;
-    Point v1, v2, v3;
-    Vector n1, n2, n3;
+
+
+    Point v[3];
+    Vector n[3];
 };
 
 }
