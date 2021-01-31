493ebd9f7e4d2e144ac13de094524b2e8e697fcb
kate00001 <kate00001@stud.uni-saarland.de>
2021-01-31 23:42:36 +0100

    Update 'rt/solids/striangle.cpp'

--- rt/solids/striangle.cpp
+++ rt/solids/striangle.cpp
@@ -7,26 +7,26 @@
 
 namespace rt {
 
-SmoothTriangle::SmoothTriangle(Point vertices[3], Vector n[3], CoordMapper* texMapper, Material* material)
+SmoothTriangle::SmoothTriangle(Point vertices[3], Vector normals[3], CoordMapper* texMapper, Material* material)
 {
-    this->n[0] = n[0];
-    this->n[1] = n[1];
-    this->n[2] = n[2];
-    this-> v[0] = vertices[0];
-    this-> v[1] = vertices[1];
-    this-> v[2] = vertices[2];
+    this->normals[0] = normals[0];
+    this->normals[1] = normals[1];
+    this->normals[2] = normals[2];
+    this-> verteces[0] = vertices[0];
+    this-> verteces[1] = vertices[1];
+    this-> verteces[2] = vertices[2];
     this->texMapper = texMapper;
     this->material = material;
 }
 
 SmoothTriangle::SmoothTriangle(const Point& v1, const Point& v2, const Point& v3, const Vector& n1, const Vector& n2, const Vector& n3, CoordMapper* texMapper, Material* material)
 {
-    this->n[0] = n1;
-    this->n[1] = n2;
-    this->n[2] = n3;
-    this-> v[0] = v1;
-    this-> v[1] = v2;
-    this-> v[2] = v3;
+    this->normals[0] = n1;
+    this->normals[1] = n2;
+    this->normals[2] = n3;
+    this-> verteces[0] = v1;
+    this-> verteces[1] = v2;
+    this-> verteces[2] = v3;
     this->texMapper = texMapper;
     this->material = material;
 }
@@ -34,60 +34,77 @@ SmoothTriangle::SmoothTriangle(const Point& v1, const Point& v2, const Point& v3
 BBox SmoothTriangle::getBounds() const {
     return BBox(
         Point(
-            std::min(std::min(v[0].x, v[1].x), v[2].x),
-            std::min(std::min(v[0].y, v[1].y), v[2].y),
-            std::min(std::min(v[0].z, v[1].z), v[2].z)
+            std::min(std::min(verteces[0].x, verteces[1].x), verteces[2].x),
+            std::min(std::min(verteces[0].y, verteces[1].y), verteces[2].y),
+            std::min(std::min(verteces[0].z, verteces[1].z), verteces[2].z)
         ),
         Point(
-            std::max(std::max(v[0].x, v[1].x), v[2].x),
-            std::max(std::max(v[0].y, v[1].y), v[2].y),
-            std::max(std::max(v[0].z, v[1].z), v[2].z)
+            std::max(std::max(verteces[0].x, verteces[1].x), verteces[2].x),
+            std::max(std::max(verteces[0].y, verteces[1].y), verteces[2].y),
+            std::max(std::max(verteces[0].z, verteces[1].z), verteces[2].z)
         )
     );
 }
 
 Intersection SmoothTriangle::intersect(const Ray& ray, float previousBestDistance) const {
+     // let's first see if the ray intersects the plane the triangle spans
 
-    Vector vec1 = this->v[1] - this->v[0];
-    Vector vec2 = this->v[2] - this->v[0];
+    // let's construct 2 Vectors that will span the plan and vec2CrossVec1find the normal as their cross product
+    Vector vec1 = this->verteces[1] - this->verteces[0];
+    //std::cout<<"Vec1 "<<vec1.x<<"  "<<vec1.y<<"  "<<vec1.z<<"\n\n";
+    Vector vec2 = this->verteces[2] - this->verteces[0];
+    //std::cout<<"Vec2 "<<vec2.x<<"  "<<vec2.y<<"  "<<vec2.z<<"\n\n";
+
     Vector normal = cross(vec1, vec2).normalize();
+
+    //check if the ray intersects the plane
+
     float rayNormal = dot(ray.d, normal);
 
-    if(rayNormal == 0.0f){
+    //std::cout<<normal.x<<"  "<<normal.y<<"  "<<normal.z<<"\n\n";
 
+    if(rayNormal == 0.0f){
+        //std::cout<<"Intersection not found\n\n";
         return Intersection::failure();
     }
 
-    float t = dot(this->v[0] - ray.o, normal) / rayNormal;
+    float t = dot(this->verteces[0] - ray.o, normal) / rayNormal;
 
     if(t > previousBestDistance ||  t < 0){
         return Intersection::failure();
     }
 
-   Point intPoint = ray.getPoint(t);
-    Vector vecP = intPoint - this->v[0];
+    //check if the intersection point is in the triangle
+    Point intPoint = ray.getPoint(t);
+    Vector vecP = intPoint - this->verteces[0];
+    
+    Vector vec1CrossVecP = cross(vec1,vecP);
+    Vector vec1CrossVec2 = cross(vec1,vec2);
 
-    if(dot(cross(vec1,vecP),  cross(vec1,vec2)) < 0.0f){
+    if(dot(vec1CrossVecP, vec1CrossVec2) < 0.0f){
         return Intersection::failure();
     }
 
-    if(dot(cross(vec2,vecP),  cross(vec2,vec1)) < 0.0f){
+    Vector vec2CrossVecP = cross(vec2,vecP);
+    Vector vec2CrossVec1 = cross(vec2,vec1);
+
+    if(dot(vec2CrossVecP, vec2CrossVec1) < 0.0f){
         return Intersection::failure();
     }
 
 
-    float denominator = cross(vec2,vec1).length();
-    float c = cross(vec1,vecP).length()/denominator;
-    float b = cross(vec2,vecP).length()/denominator;
-    float a = 1-b-c;
+    float denominator = vec2CrossVec1.length();
+    float gamma = vec1CrossVecP.length()/denominator;
+    float beta = vec2CrossVecP.length()/denominator;
+    float alpha = 1-beta-gamma;
 
-    if(b+c >= 1.0f){
+    if(beta+gamma >= 1.0f){
         return Intersection::failure();
     }
 
-    Vector New_Normal = (this->n[0] * a + this->n[1] * b + this->n[3] * c).normalize();
+    Vector pointNormal = (this->normals[0] * alpha + this->normals[1] * beta + this->normals[2] * gamma).normalize();
 
-    return Intersection(t, ray, this, New_Normal, Point(a, b, c));
+    return Intersection(t, ray, this, pointNormal, Point(alpha, beta,gamma));
     
 }
 }
